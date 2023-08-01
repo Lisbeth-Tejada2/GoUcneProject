a8986ae3756afaa1c28725281c5984eee5958e27
Latitude E7450 <Latitude E7450@LISBETH>
2023-08-01 03:41:24 -0400

    Agregando línea 24 y 25 (Se me había olvidado agregarlas y por eso daba el error mostrado al profesor el día de la presentación del proyecto)

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,26 @@
+global using GoUcneProject.Client.Servicios.Contrato;
+global using GoUcneProject.Shared;
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using GoUcneProject.Client;
+using GoUcneProject.Client.Servicios.Implementacion;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<ICategoriaService, CategoriaService>();
+builder.Services.AddScoped<IProductoService, ProductoService>();
+builder.Services.AddScoped<IRolService, RolService>();
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddScoped<IVentaService, VentaService>();
+builder.Services.AddScoped<IDashBoardService, DashBoardService>();
+
+builder.Services.AddMudServices();
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
